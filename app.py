5e95e06c0e0fdf978391b377ff9cdd365be1a7bd
Claire-ade <110232042+Claire-ade@users.noreply.github.com>
2025-11-12 16:26:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 import streamlit as st
-import py3Dmol
+import requests
 
-st.title("Visualisation 3D de molécules")
+st.title("Structure moléculaire à partir de SMILES")
 
 smiles = st.text_input("Entrez une structure SMILES :", "CCO")  # Éthanol
 
 if smiles:
-    mol_view = py3Dmol.view(width=400, height=400)
-    mol_view.addModel(smiles, "sdf")
-    mol_view.setStyle({"stick": {}})
-    mol_view.zoomTo()
-    html = mol_view._make_html()
-    st.components.v1.html(html, height=400, width=400)
+    # Utilise l'API de NCI pour générer une image PNG
+    url = f"https://cactus.nci.nih.gov/chemical/structure/{smiles}/image"
+    st.image(url, caption=f"Structure de {smiles}")
