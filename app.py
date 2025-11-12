48c7885f4e01d60943ac5431f38a0ad75ecacc88
Claire-ade <110232042+Claire-ade@users.noreply.github.com>
2025-11-12 16:23:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ st.title("Visualisation 3D de molécules")
 smiles = st.text_input("Entrez une structure SMILES :", "CCO")  # Éthanol
 
 if smiles:
-    view = py3Dmol.view(width=400, height=400)
-    view.addModel(smiles, "sdf")
-    view.setStyle({"stick": {}})
-    view.zoomTo()
-    view.show()
+    mol_view = py3Dmol.view(width=400, height=400)
+    mol_view.addModel(smiles, "sdf")
+    mol_view.setStyle({"stick": {}})
+    mol_view.zoomTo()
+    html = mol_view._make_html()
+    st.components.v1.html(html, height=400, width=400)
